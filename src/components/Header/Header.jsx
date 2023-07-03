57a9ecf6c390102031945311081fbe23e7debaa0
Tatyana <nikolina.tatyana@yandex.ru>
2023-07-03 23:51:39 +0500

    первый день марафона без ДЗ

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./Navigation/Navigation";
+import Top from "./Top/Top";
+
+const Header = () => {
+
+    return (
+        <header>
+            <Top/>
+            <Navigation/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Layout/Container/Container.jsx
@@ -0,0 +1,13 @@
+import cn from 'classnames'
+import style from './Container.module.scss';
+
+const Container = ({className, children}) => {
+
+    return (
+        <div className={cn(style.container, className)}>
+            {children}
+        </div>
+    )
+}
+
+export default Container;
