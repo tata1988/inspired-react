6ddba58503b7a836b93d51c9881567581739450d
Tatyana <nikolina.tatyana@yandex.ru>
2023-07-16 16:24:11 +0500

    добавила общую сумму в корзине

--- src/components/CartPage/Cart/Cart.jsx
+++ src/components/CartPage/Cart/Cart.jsx
@@ -4,7 +4,16 @@ import s from './Cart.module.scss';
 import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cartItems, goodsList }) => {
-    const totalPrice = 0;
+    const totalPrice = cartItems.reduce((sum, item) => {
+        const product = goodsList.find(product => product.id === item.id);
+
+        if (product) {
+            return sum + (product.price * item.count)
+        } else {
+            return sum;
+        }
+    }, 0);
+
 
     return (
         <section className={s.cart}>
